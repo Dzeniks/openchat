d368951271342497c9302625968b0ff3f5531690
Dzeniks <jan.soukenik@hotmail.com>
2024-02-16 13:55:46 +0100

    Docker prepare

--- python-api/prepare.py
+++ python-api/prepare.py
@@ -1,7 +1,7 @@
 import os
 import dotenv
 import torch
-from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
+from transformers import AutoModel, AutoTokenizer, BitsAndBytesConfig
 import runpod
 
 dotenv.load_dotenv()
@@ -13,5 +13,5 @@ model_name = os.getenv("MODEL_NAME")
 if model_name is None:
     raise Exception("MODEL_NAME environment variable is not set")
 
-model = AutoModelForCausalLM.from_pretrained(model_name)
+model = AutoModel.from_pretrained(model_name)
 
--- server-go/routes/chat/chat.go
+++ server-go/routes/chat/chat.go
@@ -4,8 +4,6 @@ import (
 	"log"
 	"net/http"
 	"server-go/lib/dotEnv"
-	"time"
-
 	"github.com/gin-gonic/gin"
 )
 
@@ -18,10 +16,8 @@ func InitChat(r *gin.RouterGroup) {
 }
 
 func ChatCompletetion(r *gin.Context) {
-	// Get the AI_URL from the environment variables
 	aiURL := dotEnv.DotEnv.AI_URL
 
-	// Make a GET request to the AI_URL
 	resp, err := http.Get(aiURL)
 	if err != nil {
 		log.Println("Error making request to AI_URL:", err)
@@ -29,20 +25,10 @@ func ChatCompletetion(r *gin.Context) {
 	}
 	defer resp.Body.Close()
 
-	// Handle the response
-	// ...
-
-	// Return the response to the client
-
-	// Save the response to the database
-
 }
 
 func ChatCompletetionTest(r *gin.Context) {
 	dummy := "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl vitae aliquam lacinia, nunc nisl luctus nunc, vitae aliquam nisl nunc eu nisl."
-
-	// Wait for 2 seconds
-	time.Sleep(2 * time.Second)
 
 	r.JSON(200, gin.H{
 		"message": "OK",
